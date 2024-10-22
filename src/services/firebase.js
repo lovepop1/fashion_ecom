b6ab62d45d03ff464f5f66abdf84dc32e1ef4fc1
Adithya Nangarath <88925242+lovepop1@users.noreply.github.com>
2024-10-23 01:14:38 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,14 +8,14 @@ import 'firebase/compat/storage';
 // 2) Fill firebaseConfig with the settings of your Firebase project.
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8SOF598TDnbM06XjhNHb3vHW0MH-7ta8",
-  authDomain: "hernan-9922c.firebaseapp.com",
-  databaseURL: "https://hernan-9922c-default-rtdb.firebaseio.com",
-  projectId: "hernan-9922c",
-  storageBucket: "hernan-9922c.appspot.com",
-  messagingSenderId: "60478048124",
-  appId: "1:60478048124:web:11d954b5b6e4cebdb146f9",
-  measurementId: "G-8D8CXF9S43"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
